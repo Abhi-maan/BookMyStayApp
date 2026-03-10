98c2f0d12cd84732afb7db6800dc8ad18a0c12d5
Abhi-maan <abhimaanverma@gmail.com>
2026-03-10 14:14:23 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,30 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This class represents the entry point of the Hotel Booking Management System.
+ * It demonstrates how a Java application starts execution using the main()
+ * method and prints a welcome message to the console.
+ *
+ * The program simulates the initialization of the booking system.
+ *
+ * @author Abhimaan
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    /**
+     * Main method – entry point of the Java application.
+     * The JVM starts program execution from here.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+
+        // Display system initialization message
+        System.out.println("System initialized successfully.");
+    }
+}
